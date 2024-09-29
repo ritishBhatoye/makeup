857af48335697bf5bdeb06bd86d6b966dddc151c
Simransingh010 <simran.ahuja0010@gmail.com>
2024-09-29 11:22:51 +0530

    gg1

--- src/components/services/serviceMainPage.tsx
+++ src/components/services/serviceMainPage.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { gsap } from 'gsap';
 
