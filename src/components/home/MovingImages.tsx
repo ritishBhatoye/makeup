5d56f30eb2021f766a3d1c02c0a716f1c169d998
Ritish Bhatoye <ritishrahul87@gmail.com>
2024-09-29 08:41:14 +0530

    DONE

--- src/components/home/MovingImages.tsx
+++ src/components/home/MovingImages.tsx
@@ -10,7 +10,7 @@ interface MovingImagesProps {
 
 function MovingImages({ images, direction }: MovingImagesProps) {
   return (
-    <div className="relative w-full sm:w-1/2 md:w-1/3 h-[300px] sm:h-[400px] md:h-[600px] overflow-hidden bg-[#f2ede8]">
+    <div className="relative w-full sm:w-1/2 lg:w-1/3 h-[300px] sm:h-[400px] lg:h-[600px] overflow-hidden bg-[#f2ede8]">
       <motion.div
         initial={{ y: direction === 'down' ? '-100%' : '100%' }}
         animate={{ y: direction === 'down' ? '100%' : '-100%' }}
@@ -20,7 +20,7 @@ function MovingImages({ images, direction }: MovingImagesProps) {
           duration: 20,
           ease: 'linear',
         }}
-        className="flex flex-col gap-2 sm:gap-3 md:gap-4 absolute right-0 left-0 sm:right-0 sm:left-auto"
+        className="flex flex-col gap-4 absolute right-0 left-0 sm:left-auto"
       >
         {images.map((src, index) => (
           <Image
@@ -29,7 +29,7 @@ function MovingImages({ images, direction }: MovingImagesProps) {
             alt={`Moving image ${index + 1}`}
             width={200}
             height={250}
-            className="rounded-xl sm:rounded-2xl shadow-md w-full sm:w-auto h-auto object-cover"
+            className="rounded-2xl shadow-md mx-auto sm:mx-0"
           />
         ))}
       </motion.div>
