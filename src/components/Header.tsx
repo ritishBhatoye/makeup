92a6deffaeb6efdd185dbe7f86fec9fa80746d1c
Simransingh010 <simran.ahuja0010@gmail.com>
2024-09-29 10:54:18 +0530

    gg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import { Menu, X } from "lucide-react";
 import { usePathname, useRouter } from 'next/navigation';
